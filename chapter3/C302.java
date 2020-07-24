5fc2392d21e4527081d58b116bcf5981fceef3ea
akif <akiftasci@yahoo.com>
2020-07-24 14:29:33 +0300

    added exercide 3.02

--- chapter3/C302.java
+++ chapter3/C302.java
@@ -1,7 +1,25 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class C302 {
 public static void main(String[] args) {
+	int a=(int)(Math.random()*10);
+	int b=(int)(Math.random()*10);
+	int c=(int)(Math.random()*10);
 	
+	System.out.println("Enter the sum of three integers:");
+	System.out.println(a+"+"+b+"+"+c+"=");
+	Scanner scan = new Scanner(System.in);
+	
+	int d = scan.nextInt();
+	int e= a+b+c;
+	if(d==e){
+		System.out.println("Your answer is correct");
+		
+	}
+	else if (d!=e){
+		System.out.println("Your answer is wrong");
+	}
 }
 }
