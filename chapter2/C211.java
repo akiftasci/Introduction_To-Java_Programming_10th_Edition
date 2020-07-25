88cea3f48d5576c787c16885702e33d782a99fca
akif <akiftasci@yahoo.com>
2020-07-25 22:52:10 +0300

    added c211

--- chapter2/C211.java
+++ chapter2/C211.java
@@ -3,15 +3,17 @@ package chapter2;
 import java.util.Scanner;
 
 public class C211 {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);	
+	
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);	
 
-		System.out.print("Enter the number of years: ");
-		int years = input.nextInt();
+			System.out.print("Enter speed and acceleration: ");
+			double speed = input.nextDouble();
+			double acceleration = input.nextDouble();
 
-		int population = 312032486 + (((31536000 / 7) - (31536000 / 13)
-								+ (31536000 / 45)) * years);
+			double length = Math.pow(speed, 2) / (2 * acceleration);
 
-		System.out.println("The population in " + years + " is " + population);
-	}
+			System.out.println("The minimum runway length for this" +
+									 " airplane is " + length);
+		}
 }
