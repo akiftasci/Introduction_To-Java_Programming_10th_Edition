41552db7f3b4aba0cd966945f59095e474428f18
akif <akiftasci@yahoo.com>
2020-07-27 15:43:52 +0300

    C2

--- chapter2/C212.java
+++ chapter2/C212.java
@@ -1,5 +1,19 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class C212 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);	
+
+		System.out.print("Enter speed and acceleration: ");
+		double speed = input.nextDouble();
+		double acceleration = input.nextDouble();
+
+		double length = Math.pow(speed, 2) / (2 * acceleration);
+
+		System.out.println("The minimum runway length for this" +
+								 " airplane is " + length);
+	}
 
 }
--- chapter2/C213.java
+++ chapter2/C213.java
@@ -1,5 +1,24 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class C213 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);	
+		final double MONTHLY_INTEREST_RATE = 0.00417;	
+
+
+		System.out.print("Enter the monthly saving amount: ");
+		double savingAmount = input.nextDouble();
+
+		double total = 100 * (1 + MONTHLY_INTEREST_RATE);
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+
+		System.out.println("After the sixth month, the account value is " + total);
+	}
 
 }
