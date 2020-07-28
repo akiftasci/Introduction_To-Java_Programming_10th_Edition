bd7201343f540355df5823151b8cb204090fbb72
akif <akiftasci@yahoo.com>
2020-07-28 10:34:56 +0300

    added C318

---
+++ chapter3/C318.java
@@ -0,0 +1,27 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class C318 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter the weight of the package: ");
+		double weight = input.nextDouble();
+
+		if (weight > 50)
+			System.out.println("The package cannot be shipped.");
+		else {
+			double costPerPound;
+			if (weight > 0 && weight <= 1)
+				costPerPound = 3.5;
+			else if (weight <= 3)
+				costPerPound = 5.5;
+			else if (weight <= 10)
+				costPerPound = 8.5;
+			else
+				costPerPound = 10.5;
+			System.out.println("Shipping cost of package is $" + costPerPound * weight);
+		}
+	}
+}
