aaba3cc3ec53239995b385082f366cff693df86d
akif <akiftasci@yahoo.com>
2020-08-02 13:36:11 +0300

    added C416

---
+++ chapter4/C416.java
@@ -0,0 +1,10 @@
+package chapter4;
+
+public class C416 {
+	public static void main(String[] args) {
+		int rand =(int)(Math.random()*25)+65; //math.random*range)+min
+		char letter = (char)(rand);
+		System.out.println(letter);
+	}
+
+}
