01a404db09225f494fbf4bf982067961eeb4a71b
akif <akiftasci@yahoo.com>
2020-08-03 13:56:08 +0300

    added C504

---
+++ chapter5/C504.java
@@ -0,0 +1,16 @@
+package chapter5;
+
+public class C504 {
+	public static void main(String[] args) {
+		int mile=1;
+		double km=0;
+		System.out.println("Miles      Kilometers");
+		while(mile<10){
+			mile++;
+			km=mile*1.609;
+			System.out.println(mile+"          "+km);
+			
+		}
+	}
+
+}
