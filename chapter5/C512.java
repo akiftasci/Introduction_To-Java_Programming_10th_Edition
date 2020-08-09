bfefa503e97c2830220f22df2462c3ca1d5304c6
akif <akiftasci@yahoo.com>
2020-08-09 16:47:37 +0300

    added C512

---
+++ chapter5/C512.java
@@ -0,0 +1,14 @@
+package chapter5;
+
+public class C512 {
+	public static void main(String[] args) {
+		int n=1;
+		while(Math.pow(n, 2)<12000){
+			n++;
+			System.out.println(n);
+			
+		}
+			
+	}
+
+}
