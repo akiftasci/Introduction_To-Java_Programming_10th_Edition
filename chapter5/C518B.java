14b89c33e6d8f2efe6933172479e3b26e2f1cd7a
akif <akiftasci@yahoo.com>
2020-08-12 11:50:14 +0300

    added C518B

---
+++ chapter5/C518B.java
@@ -0,0 +1,15 @@
+package chapter5;
+
+public class C518B {
+public static void main(String[] args) {
+	
+	System.out.println("Pattern B");
+	for(int i=6;i>=1;i--){
+		for(int x=1; x<=i;x++){
+			System.out.print(x+" ");
+		}
+		
+		System.out.println();
+	}
+}
+}
