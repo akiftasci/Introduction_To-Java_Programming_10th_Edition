bcabb33ac3e84f5cae6a8f44d89eb24e68652330
akif <akiftasci@yahoo.com>
2020-08-12 11:26:20 +0300

    added C516

---
+++ chapter5/C516.java
@@ -0,0 +1,24 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class C516 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter an integer: ");
+		int number = input.nextInt();
+		int index = 2; 
+
+		while (number / index != 1) {
+			if (number % index  == 0) {
+				System.out.print(index + ", ");
+				number /= index; 
+			}
+			else
+				index++; 
+		}
+		System.out.println(number + ".");
+	}
+
+}
