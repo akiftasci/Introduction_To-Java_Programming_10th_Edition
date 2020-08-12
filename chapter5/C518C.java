a38a4b1891643a8939fbad04bbf5395c5265c2a1
akif <akiftasci@yahoo.com>
2020-08-12 13:55:24 +0300

    fixed C518C

--- chapter5/C518C.java
+++ chapter5/C518C.java
@@ -1,7 +1,7 @@
 package chapter5;
 
 public class C518C {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		System.out.println("Pattern C");
 		for (int i = 1; i < 7; i++) {
 			for (int x = 7; x >i; x--) {
@@ -10,5 +10,18 @@ public class C518C {
 			System.out.println(i+" ");
 		}
 
-	}
+	}*/
+	public static void main(String[] args) {
+        int numberOfLines = 6;
+        System.out.println("Pattern C");
+        for (int rows = 1; rows <= numberOfLines; rows++) {
+            for (int s = numberOfLines - rows; s >= 1; s--) {
+                System.out.print("  ");
+            }
+            for (int col = rows; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
 }
