41434a1f324052055c529dd341b1825819a4e406
akif <akiftasci@yahoo.com>
2020-08-12 13:56:46 +0300

    added C518D

---
+++ chapter5/C518D.java
@@ -0,0 +1,17 @@
+package chapter5;
+
+public class C518D {
+	 public static void main(String[] args) {
+	        System.out.println("Pattern D");
+	        for (int rows = 0; rows < 6; rows++) {       
+	            for (int ws = 0; ws < rows; ws++) {      
+	                System.out.print("  ");
+	            }
+	            for (int col = 0; col < 6 - rows; col++) {    
+	                System.out.print((col + 1) + " ");
+	            }
+	            System.out.println();    
+	        }
+	    }
+
+}
