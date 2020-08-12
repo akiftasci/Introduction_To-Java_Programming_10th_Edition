f679459047dd835e99584d042dd9c27360afc2a3
akif <akiftasci@yahoo.com>
2020-08-12 11:22:46 +0300

    added C514

---
+++ chapter5/C514.java
@@ -0,0 +1,31 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class C514 {
+public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter first integer: ");
+		int n1 = input.nextInt();
+		System.out.print("Enter second integer: ");
+		int n2 = input.nextInt();
+		int gcd;
+		if (n1 < n2){
+			gcd = n1;
+		}
+		else {
+			gcd=n2;
+		}
+		
+
+		while (n1 % gcd != 0 || n2 % gcd != 0) {
+			gcd--;
+		}
+
+		System.out.println("The greatest common divisor for " + n1 +
+			" and " + n2 + " is " + gcd);
+	}
+}
+
