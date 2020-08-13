bd47f456d925f22e0db3f059736f339d90f7be1d
akif <akiftasci@yahoo.com>
2020-08-13 08:56:04 +0300

    added C535

---
+++ chapter5/C535.java
@@ -0,0 +1,11 @@
+package chapter5;
+
+public class C535 {
+public static void main(String[] args) {
+	double sum=0;
+	for(int i=1;i<626;i++){
+		sum=sum+ 1/(i+Math.sqrt(i+1));
+	}
+	System.out.println(sum);
+}
+}
