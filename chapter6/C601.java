045b1903ad45ac3381adf82cfad4967fc53056d7
akif <akiftasci@yahoo.com>
2020-08-19 14:49:09 +0300

    added C601

--- chapter6/C601.java
+++ chapter6/C601.java
@@ -4,16 +4,15 @@ public class C601 {
 public static void main(String []args ){
 	int count;
 	for(int n=1;n<101;n++){
-	System.out.print(getPentagonalNumber(n,n)+" ");
+	System.out.printf("%7d ",getPentagonalNumber(n));
 	if(n%10==0){
 		System.out.println();
 	}
 	}
 	
 }
-public static int getPentagonalNumber(int n,int count){
+public static int getPentagonalNumber(int n){
 	
-	count++;
 	int result= (n*(3*n-1))/2;
 	return result;
 }
