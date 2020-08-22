c82fa9cc3b18727be6520560982d0fc0114a6c1d
akif <akiftasci@yahoo.com>
2020-08-22 21:52:21 +0300

    added C628

---
+++ chapter6/C628.java
@@ -0,0 +1,31 @@
+package chapter6;
+
+public class C628 {
+	public static void main(String[] args) {
+		int count = 0, p=0;
+		System.out.println("p\t\t2^p-1");
+		System.out.println("____________________");
+		int i=2;
+		
+		for (i = 2; i <= 31; i++) {
+			
+				if (IsPrime(i) ) {
+			
+				p=(int)Math.pow(2, i)-1;
+				System.out.printf(i + "\t\t"+p+"\n");
+				count++;
+				
+				
+			}
+		}
+	}
+	public static boolean IsPrime(int i) {
+		boolean isprime = true;
+		for (int j = 2; j <= i / 2; j++) {
+			if (i % j == 0) {
+				isprime = false;
+			}
+		}
+		return isprime;
+	}
+}
