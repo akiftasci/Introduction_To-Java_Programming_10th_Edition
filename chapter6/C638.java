f3d2b18b029d647b4955a92f988225e7d94b0c33
akif <akiftasci@yahoo.com>
2020-08-24 11:34:37 +0300

    added C638

---
+++ chapter6/C638.java
@@ -0,0 +1,33 @@
+package chapter6;
+
+public class C638 {
+public static void main(String[] args) {
+	for(int i=1;i<101;i++){
+	System.out.print(getRandomUpperCaseLetter()+" ");
+	if(i%10==0){
+		System.out.println();
+	}
+}
+	for(int i=1;i<101;i++){
+		System.out.print(getRandomDigitCharacter()+" ");
+		if(i%10==0){
+			System.out.println();
+		}
+	}
+	}
+public static char getRandomCharacter(char ch1, char ch2) {
+ return (char)(ch1 + Math.random() * (ch2 - ch1 + 1));
+}
+public static char getRandomLowerCaseLetter() {
+ return getRandomCharacter('a', 'z');
+}
+public static char getRandomUpperCaseLetter() {
+ return getRandomCharacter('A', 'Z');
+}
+public static char getRandomDigitCharacter() {
+ return getRandomCharacter('0', '9');
+}
+public static char getRandomCharacter() {
+return getRandomCharacter('\u0000', '\uFFFF');
+}
+}
