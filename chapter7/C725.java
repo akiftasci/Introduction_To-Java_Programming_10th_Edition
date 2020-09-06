a21e9a2a98d34be81883cd616960d404391a0fe2
akif <akiftasci@yahoo.com>
2020-09-06 21:15:04 +0300

    added C725

--- chapter7/C725.java
+++ chapter7/C725.java
@@ -1,5 +1,41 @@
 package chapter7;
 
-public class C725 {
+import java.util.Scanner;
 
+public class C725 {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter your a, b and c");
+	double  [] abc = new double[3];
+	int inp;
+	for(int i=0;i<abc.length;i++){
+		inp=input.nextInt();
+		abc[i]=inp;
+	}
+double x1, x2 , delta;
+	
+	delta = (abc[1]*abc[1])-(4*abc[0]*abc[2]);
+	x1= (-abc[1]+Math.sqrt(delta))/2*abc[0];
+	x2= (-abc[1]-Math.sqrt(delta))/2*abc[0];
+	double [] roots = new double[2];
+	roots[0]=x1;
+	roots[1]=x2;
+	
+	System.out.println(solveQuadratic(abc, roots));
+	
+}
+public static int solveQuadratic(double[] eqn, double[] roots){
+	
+	if ( delta>0){
+		System.out.println("The equation has two roots "+ x1 +"and"+x2);
+	}
+	
+	else if (delta==0){
+		System.out.println("The equation has one root:"+x1);
+	}
+	else {
+		System.out.println("The equation has no real roots");
+	}
+	
+}
 }
