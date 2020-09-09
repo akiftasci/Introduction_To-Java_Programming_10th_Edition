3dff073f0bd37d54cfa197531934f1df62b8db0b
akif <akiftasci@yahoo.com>
2020-09-09 12:59:47 +0300

    added C730

---
+++ chapter7/C730.java
@@ -0,0 +1,44 @@
+package chapter7;
+
+import java.util.Scanner;
+
+public class C730 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number of values: ");
+		int number = input.nextInt();
+		int[] arr = new int[number];
+		System.out.println("Enter the values: ");
+		int value;
+		for (int i = 0; i < arr.length; i++) {
+			value = input.nextInt();
+			arr[i] = value;
+		}
+		consecutive(arr);
+	}
+
+	private static void consecutive(int[] arr) {
+		int temp;
+		int count = 0;
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		int[] tempArr = new int[max];
+		for (int j = 0; j < arr.length; j++) {
+			temp = arr[j];
+			tempArr[temp - 1]++;
+		}
+		for (int j = 0; j < tempArr.length; j++) {
+			if (tempArr[j] == 4) {
+				System.out.println("The list has consecutive fours");
+				break;
+			}
+
+		}
+		System.out.println("The list does not have consecutive fours");
+	}
+
+}
