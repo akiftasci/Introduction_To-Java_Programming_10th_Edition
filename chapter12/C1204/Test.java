3e9b86f0692ef4c16d6babf7cd16d2c10932829a
akif <akiftasci@yahoo.com>
2020-10-19 12:10:32 +0300

    added C1204

---
+++ chapter12/C1204/Test.java
@@ -0,0 +1,11 @@
+package chapter12.C1204;
+
+public class Test {
+	public static void main(String[] args) {
+		 Loan loan1 = new Loan(0, 5, 5);
+		 Loan loan2 = new Loan(5, 0, 5);
+		 Loan loan3 = new Loan(5, 5, 0);
+	  
+	}
+
+}
